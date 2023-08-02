7bc5de4bfb948a2480e5faf5dbdbafab1030da39
Xchel <Xchel@DESKTOP-F0KOPP8>
2023-08-02 00:17:27 -0600

    Proyecto base

---
+++ src/main/java/com/dustbuster/dustbusterApi/Model/Bean/JwtResponseBean.java
@@ -0,0 +1,13 @@
+package com.dustbuster.dustbusterApi.Model.Bean;
+
+import com.dustbuster.dustbusterApi.Entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponseBean {
+    private String token;
+    private String type = "Bearer";
+    private Usuario usuario;
+}
